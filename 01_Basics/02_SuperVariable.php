af30fdea59c2a6f7a90026853de9e42fa433469a
Azhar Khan <azar123khan456@gmail.com>
2024-04-20 22:34:29 +0530

    added supervariable notes

---
+++ 01_Basics/02_SuperVariable.php
@@ -0,0 +1,15 @@
+<?php 
+//! Notes
+//Super Global Variables are built-in variables in PHP that are always accessible, regardless of scope. These variables are used to collect data from forms, cookies, and URLs.
+//The PHP superglobal variables are:
+    //* $GLOBALS
+    //* $_SERVER
+    //* $_REQUEST
+    //* $_POST
+    //* $_GET
+    //* $_FILES
+    //* $_ENV
+    //* $_COOKIE
+    //* $_SESSION
+
+?>
